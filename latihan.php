2bc8b4d79252d355fb03be788bce8de5f72f5e72
Reyhan05 <kingreyhan298@gmail.com>
2023-01-18 13:40:17 +0700

    [feat]: add count year born

--- latihan.php
+++ latihan.php
@@ -1,9 +1,20 @@
 <?php
+    // latihan hitung bb ideal
     $tb='178';
     $bbn=$tb-100;
     $bbi=$bbn-(0.1*$bbn);
     
     echo "Tinggi Badan Anda : $tb cm<br>";
     echo "Jadi Berat Badan Ideal Anda : $bbi kg";
+
+    echo "<br>";
     
+    // latihan hitung umur
+    $thn='2023';
+    $thn_lahir='2005';
+    $umur=$thn-$thn_lahir;
+
+    echo "Tahun ini usia saya : $umur tahun<br>";
+
+
 ?>
