47f14e32ab6b8417172fd19d5b598786a7d14390
Reyhan05 <kingreyhan298@gmail.com>
2023-01-19 08:52:16 +0700

    task ganjil genap

--- belajar3.php
+++ belajar3.php
@@ -10,4 +10,14 @@
         echo 'Gagal';
     }
 
+    // for if else odd even
+    for ($i=1; $i <= 21; $i++) {
+        if ($i % 2 == 0) {
+            echo "ganjil <br/>$i";
+        } else {
+            echo "genap <br/>$i";
+        }
+    }
+
+
 ?>
