9f59a5ef16a2b5283a54a92a6ea826570d7d2e70
Reyhan05 <kingreyhan298@gmail.com>
2023-02-10 09:22:11 +0700

    create form

---
+++ register.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<form method="post">
+    <input type="text" name="username" autocomplete="off" placeholder="Masukan Username..."/>
+    <br/>
+    <input type="email" name="email" autocomplete="off" placeholder="Masukan Email..."/>
+    <br/>
+    <input type="text" name="nama" autocomplete="off" placeholder="Masukan Nama Lengkap..."/>
+    <br/>
+    <input type="password" name="password" autocomplete="off" placeholder="Masukan Password..."/>
+    <br/>
+    <button type="submit" name="submit"></button>
+</form>
