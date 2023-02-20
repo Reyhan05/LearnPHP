30d9057c36f46535f90c5d52f4f8d9dffa2541cd
Reyhan05 <kingreyhan298@gmail.com>
2023-02-20 13:30:54 +0700

    add window

--- hapus_admin.php
+++ hapus_admin.php
@@ -6,16 +6,18 @@
     $id = $_GET['id'];
 
     // membuat query untuk menghapus data berdasarkan parameter id
-    $result = mysqli($connect, "DELETE FROM student WHERE id='$id'");
+    $result = mysqli_query($connect, "DELETE FROM student WHERE id='$id'");
 
     // mengecek apakah query berhasil di eksekusi
     if ($result){
-        echo '<script>
-              alert("Data berhasil di hapus !");
-              </script>';
+        echo "<script>
+              alert('Data berhasil di hapus !')
+              window.location.href = 'halaman_admin.php'
+              </script>";
     } else {
+        // jika query gagal di eksekusi
         echo '<script>
-              alert(" gagal Maning !");  
+              alert(" gagal Maning !")
               </script>';
     }
 ?>
