524a6c0164836633862e251d8a3de8fdff0a5862
Reyhan05 <kingreyhan298@gmail.com>
2023-02-20 14:01:43 +0700

    create form in edit admin

--- edit_admin.php
+++ edit_admin.php
@@ -10,23 +10,38 @@
     
 
     <?php
-    // fungsi edit data
-    
+        // fungsi edit data
+        include "koneksi.php";
+
+        // memanggil parameter id yang akan di edit
+        $id = $_GET['id'];
+
+        // membuat query untuk mengedit data berdasarkan parameter id
+        $result = mysqli_query($connect, "SELECT * FROM student WHERE id= $id");
+        
+        // membuat looping object berdasarkan parameter id
+        while($edit = mysqli_fetch_array($result)){
+            $nik = $edit['nik'];
+            $nama = $edit['nama'];
+            $kelas = $edit['kelas'];
+            $jurusan = $edit['jurusan'];
+            $alamat = $edit['alamat'];
+        }
     ?>
     
     <form method="post">
         <table width="25%" border="0">
             <tr>
                 <td>Nik</td>
-                <td><input type="number" name="nik"></td>
+                <td><input type="number" name="nik" value=<?php echo $nik;?>>></td>
             </tr>
             <tr>
                 <td>Nama</td>
-                <td><input type="text" name="nama"></td>
+                <td><input type="text" name="nama" value=<?php echo $nama;?>>></td>
             </tr>
             <tr>
                 <td>Kelas</td>
-                <td><input type="number" name="kelas"></td>
+                <td><input type="number" name="kelas" value=<?php echo $kelas;?>></td>
             </tr>
             <tr>
                 <td>Jurusan</td>
@@ -42,14 +57,12 @@
             <tr>
                 <td>Alamat</td>
                 <td>
-                    <textarea name="alamat" rows="5"></textarea>
+                    <textarea name="alamat" rows="5"><?php echo $alamat;?></textarea>
                 </td>
             </tr>
             <tr>
-                <td></td>
-                <td>
-                    <button type="submit" name="update">Update</button>
-                </td>
+                <td><input type="hidden" name="id" value=<?php echo $_GET['id'];?>></td>
+                <td><button type="submit" name="update">Update</button></td>            
             </tr>
         </table>
      </form>
